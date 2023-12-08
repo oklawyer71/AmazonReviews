33efa346c9f46202bb970248fb4b2f935a3a9a36
oklawyer71 <121524671+oklawyer71@users.noreply.github.com>
2023-12-07 20:02:13 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
+
 import numpy as np
+import pandas as pd
 from flask import Flask, request, render_template
+from sklearn import preprocessing
 import pickle
 
 app = Flask(__name__)
-
-model = pickle.load(open('lda_model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -12,12 +14,21 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
-    features = [np.array(int_features)]  
-    prediction = model.predict(features) 
-    result = prediction[0]
+    feature_list = request.form.to_dict()
+    feature_list = list(feature_list.values())
+    feature_list = list(map(int, feature_list))
+    final_features = np.array(feature_list).reshape(1, 12) 
+    
+    prediction = model.predict(final_features)
+    output = int(prediction[0])
+    if output == 1:
+        text = ">50K"
+    else:
+        text = "<=50K"
 
-    return render_template('index.html', prediction=result)
+    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+view rawHerokuIncomePredictionApp hosted with ❤ by GitHub
