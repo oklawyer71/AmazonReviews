95bb16cfa0a832c4a6cc08ec6e498c47fdfbab0e
oklawyer71 <121524671+oklawyer71@users.noreply.github.com>
2023-12-28 16:29:01 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -26,9 +26,27 @@ def intertopic():
     textout = "Intertopic Map"
     return render_template("intertopic.html", textout=textout)
 
+# instantiate Milestone 1's page
+@app.route("/milestone1")
+def milestone1():
+    textout = "Milestone 1 - Exploratory Data Analysis (EDA)"
+    return render_template("milestone1.html", textout=textout)
+
+# instantiate Milestone 2's page
+@app.route("/milestone2")
+def milestone2():
+    textout = "Milestone 2 - Bidirectional Encoder Representations from Transformers (BERT)"
+    return render_template("milestone2.html", textout=textout)
 
 # instantiate Milestone 3's page
 @app.route("/milestone3")
 def milestone3():
     textout = "Milestone 3 - Sentiment Analysis"
     return render_template("milestone3.html", textout=textout)
+
+
+# instantiate Milestone 3's page
+@app.route("/milestone4")
+def milestone4():
+    textout = "Milestone 4 - Final Project"
+    return render_template("milestone4.html", textout=textout)
