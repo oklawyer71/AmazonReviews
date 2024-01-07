1b0f4fc9a93ef052968c6f0d60e17914c41f6054
oklawyer71 <121524671+oklawyer71@users.noreply.github.com>
2024-01-07 10:54:49 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,80 @@
 from flask import current_app as app
 from flask import render_template, request
 
-
+# instantiate index page
 @app.route("/")
+def index():
+    print(os.getcwd())
+    return render_template("./home.html")
+
+@app.route("/dashboard")
 def home():
     """Home page of Flask Application."""
     return render_template(
-        "index.jinja2",
+        "dashboard.jinja2",
         title="Plotly Dash Flask Tutorial",
         description="Embed Plotly Dash into your Flask applications.",
         template="home-template",
         body="This is a homepage served with Flask.",
         base_url=request.base_url,
     )
+
+# instantiate dataset page
+@app.route("/dataset")
+def dataset():
+    textout = "Dataset Used"
+    return render_template("dataset.html", textout=textout)
+
+# instantiate data preparation page
+@app.route("/data-preparation")
+def dataprep():
+    textout = "Data Preparation"
+    return render_template("data-preparation.html", textout=textout)
+
+# instantiate data modelling page
+@app.route("/data-modelling1")
+def datamodel1():
+    textout = "Data Modelling 1"
+    return render_template("data-modelling1.html", textout=textout)
+
+# instantiate data modelling page
+@app.route("/data-modelling2")
+def datamodel2():
+    textout = "Data Modelling 2"
+    return render_template("data-modelling2.html", textout=textout)
+    
+# instantiate intertopic page
+@app.route("/intertopic")
+def intertopic():
+    textout = "Intertopic Map"
+    return render_template("intertopic.html", textout=textout)
+
+# instantiate Milestone 1's page
+@app.route("/milestone1")
+def milestone1():
+    textout = "Milestone 1 - Exploratory Data Analysis (EDA)"
+    return render_template("milestone1.html", textout=textout)
+
+# instantiate Milestone 2's page
+@app.route("/milestone2")
+def milestone2():
+    textout = "Milestone 2 - Bidirectional Encoder Representations from Transformers (BERT)"
+    return render_template("milestone2.html", textout=textout)
+
+# instantiate Milestone 3's page
+@app.route("/milestone3")
+def milestone3():
+    textout = "Milestone 3 - Sentiment Analysis"
+    return render_template("milestone3.html", textout=textout)
+
+# instantiate Milestone 4's page
+@app.route("/milestone4")
+def milestone4():
+    textout = "Milestone 4 - Final Project"
+    return render_template("milestone4.html", textout=textout)
+
+# instantiate Citations page
+@app.route("/citations")
+def citations():
+    textout = "Citations"
+    return render_template("citations.html", textout=textout)
