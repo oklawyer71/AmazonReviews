c1e49370e97e6566bd416910570085dcd67e1313
oklawyer71 <121524671+oklawyer71@users.noreply.github.com>
2024-01-07 19:04:25 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -46,28 +46,16 @@ def intertopic():
     return render_template("intertopic.html", textout=textout)
 
 # instantiate Milestone 1's page
-@app.route("/milestone1")
-def milestone1():
-    textout = "Milestone 1 - Exploratory Data Analysis (EDA)"
-    return render_template("milestone1.html", textout=textout)
+@app.route("/individual-project")
+def individualproject():
+    textout = "Individual Project"
+    return render_template("individual-project.html", textout=textout)
 
 # instantiate Milestone 2's page
-@app.route("/milestone2")
-def milestone2():
-    textout = "Milestone 2 - Bidirectional Encoder Representations from Transformers (BERT)"
-    return render_template("milestone2.html", textout=textout)
-
-# instantiate Milestone 3's page
-@app.route("/milestone3")
-def milestone3():
-    textout = "Milestone 3 - Sentiment Analysis"
-    return render_template("milestone3.html", textout=textout)
-
-# instantiate Milestone 4's page
-@app.route("/milestone4")
-def milestone4():
-    textout = "Milestone 4 - Final Project"
-    return render_template("milestone4.html", textout=textout)
+@app.route("/milestones")
+def milestones():
+    textout = "Milestones"
+    return render_template("milestones.html", textout=textout)
 
 # instantiate Citations page
 @app.route("/citations")
