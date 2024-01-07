2b647086d9ad64056871075ae997799cef5df8d5
oklawyer71 <121524671+oklawyer71@users.noreply.github.com>
2024-01-07 10:59:51 -0400

    Update dashboard.py

--- app/plotlydash/dashboard.py
+++ app/plotlydash/dashboard.py
@@ -1,4 +1,11 @@
-from dash import Dash
+"""Instantiate a Dash app."""
+import dash
+from dash import dash_table
+from dash import dcc
+from dash import html
+
+from .data import create_dataframe
+from .layout import html_layout
 
 
 def init_dashboard(server):
@@ -7,11 +14,42 @@ def init_dashboard(server):
         server=server,
         routes_pathname_prefix='/dashapp/',
         external_stylesheets=[
-            '/static/css/styles.css',
+            '/static/css/style.css',
+            '/static/css/normalize.css',
+            '/static/css/skeleton.css',
         ]
     )
 
-    # Create Dash Layout
-    dash_app.layout = html.Div(id='dash-container')
+    # Load DataFrame
+    df = create_dataframe()
 
+    # Custom HTML layout
+    dash_app.index_string = html_layout
+
+    # Create Layout
+    dash_app.layout = html.Div(
+        children=[
+            dcc.Graph(
+                id="histogram-graph",
+                figure={
+                    "data": [
+                        {
+                            "x": df["complaint_type"],
+                            "text": df["complaint_type"],
+                            "customdata": df["key"],
+                            "name": "311 Calls by region.",
+                            "type": "histogram",
+                        }
+                    ],
+                    "layout": {
+                        "title": "NYC 311 Calls category.",
+                        "height": 500,
+                        "padding": 150,
+                    },
+                },
+            ),
+            create_data_table(df),
+        ],
+        id="dash-container",
+    )
     return dash_app.server
